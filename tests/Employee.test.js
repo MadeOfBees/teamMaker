a717c64a6ab317843568c0ecd3474f9a9cab7522
Bee <xronsredstone@gmail.com>
2022-10-25 17:10:56 -0400

    first commit

---
+++ tests/Employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require('../lib/Employee')
+//require our class Eeployee
+describe('Employee', () => {
+    const testEmployee = new Employee(`Bee`, 1, `ben@goat.net`)
+
+    it(`Instantiates as a class object`,()=>{
+expect(typeof(testEmployee)).toBe(`object`);
+})
+
+    it(`Has three properties`, ()=>{
+        expect(Object.keys(testEmployee).length).toBe(3);
+    })
+
+    it(`Has the method getName return the property name`, ()=>{
+        expect(testEmployee.getName()).toBe(`Bee`);
+    })
+
+
+
+
+
+})
